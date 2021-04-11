662ff01d452de674836860cd9b7968140946d697
respel <respel@respel.com>
2021-04-11 04:01:09 +0200

    testing again

--- src/Chess22kDotNet/ChessBoard.cs
+++ src/Chess22kDotNet/ChessBoard.cs
@@ -5,7 +5,7 @@ using Chess22kDotNet.Eval;
 using Chess22kDotNet.Move;
 using static Chess22kDotNet.ChessConstants;
 
-namespace Chess22kDotNet
+namespace Chess22kDotNet2
 {
     public class ChessBoard
     {
